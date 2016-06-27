bcd0c81eec815f61faf961a261d87300bcee69bb
q10myhsg <q10myhsgwlczrwh@163.com>
2016-06-27 11:31:56 +0800

    fangcheng

---
+++ src/home/common/util/file/MyFileWriter.java
@@ -0,0 +1,54 @@
+package home.common.util.file;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
+
+public class MyFileWriter {
+	
+	 FileWriter fw ;
+	 private static final int lineCount=40000;
+	 private  int count=0;
+	 private String rootPath="";
+	public MyFileWriter(String path)
+	{
+			try {
+				rootPath=path;
+				if(path.contains("\\"))
+				{
+					rootPath=rootPath.replace("\\", "-");
+				}
+				fw = new FileWriter(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+	}
+	public void write(String line)
+	{
+		try {
+			count++;			
+			fw.write(line);
+			fw.flush();
+			if(count==lineCount)
+			{
+				close();
+				count=0;
+				fw= new FileWriter(rootPath+new Date().getTime());
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void close()
+	{
+		try {
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
